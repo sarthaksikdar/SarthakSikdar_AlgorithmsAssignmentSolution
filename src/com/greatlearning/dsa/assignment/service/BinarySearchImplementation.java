717e554a549623d18cc730dc2946a63a35c78d76
Sarthak Sikdar <sarthaksikdar@gmail.com>
2022-04-03 14:18:24 +0530

    SarthakSikdar_AlgorithmsAssignmentSolution

---
+++ src/com/greatlearning/dsa/assignment/service/BinarySearchImplementation.java
@@ -0,0 +1,30 @@
+package com.greatlearning.dsa.assignment.service;
+
+public class BinarySearchImplementation {
+
+
+	public void searchValue(double priceArray[], double key) {
+		int left = 0;
+		int right = priceArray.length - 1;
+		int mid = left + (right - left) / 2;
+
+		while (left <= right) {
+
+			if (key < priceArray[mid]) {
+				right = mid - 1;
+			} else if (key > priceArray[mid]) {
+				left = mid + 1;
+
+			} else {
+				System.out.println("Stock of value " + key + " is present ");
+				break;
+			}
+			mid = left + (right - left) / 2;
+		}
+
+		if (left > right) {
+			System.out.println("Value not found");
+		}
+	}
+
+}
